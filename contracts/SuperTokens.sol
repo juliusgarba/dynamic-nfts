2e5707195e4068e6a6ac280d4d69424d42888f6c
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2022-10-19 18:09:32 +0400

    fixes and improvements

--- contracts/SuperTokens.sol
+++ contracts/SuperTokens.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
@@ -17,7 +17,6 @@ contract SuperTokens is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         uint256 likeCount;
         uint256 happyCount;
         uint256 angryCount;
-        uint256 commentCount;
     }
 
     struct Comment {
@@ -25,41 +24,50 @@ contract SuperTokens is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         string content;
     }
 
-    mapping(uint256 => mapping(address => uint256)) internal reactions;
-    mapping(uint256 => Comment[]) internal comments;
-    mapping(uint256 => Token) internal tokens;
+    mapping(uint256 => mapping(address => uint256)) private reactions;
+    mapping(uint256 => Comment[]) private comments;
+    mapping(uint256 => Token) private tokens;
+
+    enum Reaction{
+        UNDEFINED,
+        LIKES,
+        HAPPY,
+        ANGRY
+    }
 
     constructor() ERC721("Super Tokens", "SUT") {}
 
-    function mint(string memory _tokenURI, uint256 id) private {
+
+    modifier exists(uint _tokenId){
+        require(_exists(_tokenId), "Query of nonexistent token");
+        _;
+    }
+
+    /// @dev mints an NFT character
+    /// @notice Token's uri needs to be a nonempty and valid value
+    function mintCharacter(string calldata _tokenURI) public  {
+        require(bytes(_tokenURI).length > 8, "Enter a valid token uri"); //ipfs uri generated on the frontend starts with "https://"
+        uint256 id = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        // Mint new token and add to blockchain
         _safeMint(msg.sender, id);
         _setTokenURI(id, _tokenURI);
-    }
-
-    // mint an NFT character
-    function mintCharacter(string memory _tokenURI) public {
-        require(bytes(_tokenURI).length > 7, "Enter a valid token uri"); //ipfs uri starts with "ipfs://"
-        uint256 id = _tokenIdCounter.current();
-        // Mint new token and add to blockchain
-        mint(_tokenURI, id);
         // Create token object
-        tokens[id] = Token(id, 0, 0, 0, 0);
-        _tokenIdCounter.increment();
+        tokens[id] = Token(id, 0, 0, 0);
+        
     }
 
-    // Add a reaction to a token
-    function addReaction(uint256 tokenId, uint256 reactionId) public {
+    /// @dev Add a reaction to a token
+    /// @notice You can only add one reaction per Character. The choosen reaction is permanent and can't be modified
+    /// @notice Reactions available are: Like, Happy and Angry. Choosing other types of reactions will be rejected
+    function addReaction(uint256 tokenId, uint256 reactionId) public exists(tokenId){
         // 1 - Like reaction
         // 2 - Happy reaction
         // 3 - Angry reaction
 
         require(
-            reactionId == 1 || reactionId == 2 || reactionId == 3,
+            reactionId == uint(Reaction.LIKES) || reactionId == uint(Reaction.HAPPY) || reactionId == uint(Reaction.ANGRY),
             "Invalid reaction entered"
-        );
-        require(
-            tokenId < _tokenIdCounter.current(),
-            "Invalid token ID entered"
         );
         require(
             reactions[tokenId][msg.sender] == 0,
@@ -79,22 +87,17 @@ contract SuperTokens is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         reactions[tokenId][msg.sender] = reactionId;
     }
 
-    // Add a new comment to token
-    function addComment(uint256 tokenId, string calldata content) public {
-        require(
-            tokenId < _tokenIdCounter.current(),
-            "Invalid token ID entered"
-        );
-        Token storage token = tokens[tokenId];
-        Comment memory newComment = Comment(msg.sender, content);
-        comments[tokenId].push(newComment);
-        token.commentCount += 1;
+    /// @dev Add a new comment to token
+    function addComment(uint256 tokenId, string calldata content) public exists(tokenId) {
+        require(bytes(content).length > 0, "Content for comment can't be empty");
+        comments[tokenId].push(Comment(msg.sender, content));
     }
 
     // Get token character from contract
     function getToken(uint256 _tokenId)
         public
         view
+        exists(_tokenId)
         returns (
             string memory _tokenURI,
             address owner,
@@ -111,28 +114,18 @@ contract SuperTokens is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         owner = ownerOf(_tokenId);
     }
 
-    // Get comments of a particular token character
+    /// @dev Get comments of a particular token character
     function getComment(uint256 _tokenId)
         public
         view
+        exists(_tokenId)
         returns (Comment[] memory)
     {
-        Token memory _token = tokens[_tokenId];
-        uint256 commentCount = _token.commentCount;
-        Comment[] memory _comments = new Comment[](commentCount);
-        for (uint256 i = 0; i < commentCount; i++) {
-            _comments[i] = comments[_tokenId][i];
-        }
-
-        return _comments;
+        return comments[_tokenId];
     }
 
-    // Check which reaction a user made on a token character
-    function whichReaction(uint256 _tokenId) public view returns (uint256) {
-        require(
-            _tokenId < _tokenIdCounter.current(),
-            "Invalid token ID entered"
-        );
+    /// @dev Check which reaction a user made on a token character
+    function whichReaction(uint256 _tokenId) public view exists(_tokenId) returns (uint256) {
         return reactions[_tokenId][msg.sender];
     }
 
